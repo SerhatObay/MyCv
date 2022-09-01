eeb09ea53f155bdf6b617249a5313fc6dd553184
Serhat Obay <serhat.obay2121@gmail.com>
2022-09-01 20:36:10 +0300

    asset blade

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -16,11 +16,11 @@
     <button class="btn btn-white btn-share ml-auto mr-3 ml-md-0 mr-md-auto"><img src="{{asset('assets/images/share.svg')}}" alt="share" class="btn-img">
         SHARE</button>
     <nav class="collapsible-nav" id="collapsible-nav">
-        <a href="{{route('index')}}" class="nav-link active">HOME</a>
-        <a href="{{route('resume')}}" class="nav-link">RESUME</a>
-        <a href="{{route('portfolio')}}" class="nav-link">PORTFOLIO</a>
-        <a href="{{route('blog')}}" class="nav-link">BLOG</a>
-        <a href="{{route('contact')}}" class="nav-link">CONTACT</a>
+        <a href="{{route('index')}}" class="nav-link {{Route::is('index') ? 'active' : ''}}">HOME</a>
+        <a href="{{route('resume')}}" class="nav-link {{Route::is('resume') ? 'active' : ''}}">RESUME</a>
+        <a href="{{route('portfolio')}}" class="nav-link" {{Route::is('portfolio') ? 'active' : ''}}>PORTFOLIO</a>
+        <a href="{{route('blog')}}" class="nav-link" {{Route::is('blog') ? 'active' : ''}}>BLOG</a>
+        <a href="{{route('contact')}}" class="nav-link {{Route::is('contact') ? 'active' : ''}}">CONTACT</a>
     </nav>
     <button class="btn btn-menu-toggle btn-white rounded-circle" data-toggle="collapsible-nav"
             data-target="collapsible-nav"><img src="{{asset('assets/images/hamburger.svg')}}" alt="hamburger"></button>
